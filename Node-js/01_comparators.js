e5585a00397beff289ef13c75bfb8df0b1aef039
Tushar <134492947+tusharnagphase@users.noreply.github.com>
2023-11-01 11:50:17 +0000

    comparators

---
+++ Node-js/01_comparators.js
@@ -0,0 +1,8 @@
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+console.log("2" ==2 );//true
+console.log(undefined > 0);//false
+console.log(undefined ==0);//false
+console.log(undefined >=0);//false
+console.log("02">1);//true
--- Node-js/01_conversion.js
+++ Node-js/01_conversion.js
@@ -1,6 +1,16 @@
-let score="23a"
-console.log(typeof score);
-console.log(typeof(score));
-let changeToNumber=Number(score)
-console.log(changeToNumber);
-console.log(typeof changeToNumber);
+// let score="23a"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let changeToNumber=Number(score)
+// console.log(changeToNumber);
+// console.log(typeof changeToNumber);
+// let a=3
+// let b=a++
+// console.log(a);
+// console.log(b);
+console.log(3+3+"3");
+console.log("1"+1+1);
+console.log(1+"1");
+console.log("1"+1);
+console.log("3"+3+"3");
+console.log("3"+3+3);
