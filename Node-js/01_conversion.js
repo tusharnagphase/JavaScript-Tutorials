f13ab5cf58174565659842f111eb7855724309d2
Tushar <134492947+tusharnagphase@users.noreply.github.com>
2023-11-01 06:20:09 +0000

    conversion_basics

---
+++ Node-js/01_conversion.js
@@ -0,0 +1,6 @@
+let score="23a"
+console.log(typeof score);
+console.log(typeof(score));
+let changeToNumber=Number(score)
+console.log(changeToNumber);
+console.log(typeof changeToNumber);
---
+++ Node-js/01_variables.js
@@ -0,0 +1,9 @@
+const accountNumber=12345;
+let accId="23";
+var accEmail="tushar@g.com";
+accPass="9898";
+//accountNumber=2341
+accId=89
+accEmail="dsvsvs@g.com"
+accPass=789546
+console.table([accountNumber,accEmail,accId,accPass]);
